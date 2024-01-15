67bc2e9a6c97fd550884628288c9a3e3dbe2367e
Luisw121 <lafernandez@elpuig.xeill.net>
2024-01-15 19:38:39 +0100

    Ultimos cambios

--- src/main/java/com/example/consumingrest/LiveScoreService.java
+++ src/main/java/com/example/consumingrest/LiveScoreService.java
@@ -1,10 +1,17 @@
 package com.example.consumingrest;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.http.HttpHeaders;
+import java.awt.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.springframework.http.HttpHeaders;
+
 
 @Service
 public class LiveScoreService {
@@ -24,7 +31,22 @@ public class LiveScoreService {
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-RapidAPI-Key", apiKey);
         headers.set("X-RapidAPI-Host", apiHost);
-        headers
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Void> httpEntity = new HttpEntity<>(headers);
+
+        try {
+            ResponseEntity<LiveScores> responseEntity = restTemplate.exchange(
+                    apiUrl,
+                    HttpMethod.GET,
+                    httpEntity,
+                    LiveScores.class
+            );
+            return responseEntity.getBody();
+        }catch (HttpClientErrorException | HttpServerErrorException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error en la llamada a la API - HttpStatus: " + e.getStatusCode() + ", Response: " + e.getResponseBodyAsString());
+        }
     }
 
 }
